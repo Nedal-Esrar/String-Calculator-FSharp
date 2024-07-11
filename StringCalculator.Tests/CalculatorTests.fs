4e49b0f29020647300a535455df5c5cb5bd1fb4a
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2024-07-11 09:16:13 +0300

    Edit tests for handling an unknown amount of numbers
    Removed the 'more than 2 numbers' test and add test cases for the valid string condition.

--- StringCalculator.Tests/CalculatorTests.fs
+++ StringCalculator.Tests/CalculatorTests.fs
@@ -14,6 +14,8 @@ let ``Add should throw NullReferenceException when null is passed`` () =
 [<InlineData("2", 2)>]
 [<InlineData("1,2", 3)>]
 [<InlineData("1,3", 4)>]
+[<InlineData("1,2,3,4", 10)>]
+[<InlineData("1,10,20,4", 35)>]
 let ``Add should return the expected result when a valid string is passed`` (numbers : string, expected : int) =
    Add numbers |> should equal expected
    
@@ -25,8 +27,3 @@ let ``Add should return the expected result when a valid string is passed`` (num
 [<InlineData("1b,2")>]
 let ``Add should throw FormatException when an invalid string is passed`` (numbers : string) =
    (fun () -> Add numbers |> ignore) |> should throw typeof<System.FormatException>
-   
-[<Theory>]
-[<InlineData("1,2,3,4")>]
-let ``Add should throw ArgumentException when more than 2 numbers exists`` (numbers : string) =
-   (fun () -> Add numbers |> ignore) |> should throw typeof<System.ArgumentException>
