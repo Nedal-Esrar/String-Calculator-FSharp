38844b29fd7211152d65a997a9dbdd225f472b11
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2024-07-11 11:13:00 +0300

    Refactor `Add` function and support custom delimiter

--- StringCalculator/Calculator.fs
+++ StringCalculator/Calculator.fs
@@ -3,16 +3,21 @@ module StringCalculator.Calculator
 open System
 open StringCalculator.Utils
 
-let Add (numbers : string) : int =
-  if numbers = null then
-    raise (NullReferenceException())
+let private ParseDelimiters (numbers: string) : char[] * string =
+  if numbers.StartsWith("//") then
+    let delimiter = numbers[2]
+    let numbers = numbers.Substring(4)
 
-  if String.IsNullOrEmpty numbers then
-    0
+    [| delimiter |], numbers
   else
-    let defaultDelimiters = [| ','; '\n' |]
+    [| ','; '\n' |], numbers
 
-    numbers.Split defaultDelimiters
+let Add (numbers: string) : int =
+  match numbers with
+  | null -> raise (NullReferenceException())
+  | "" -> 0
+  | _ ->
+    let delimiters, numbers = ParseDelimiters numbers
+    numbers.Split delimiters
     |> Array.map ParseInt
     |> Array.sum
-    
